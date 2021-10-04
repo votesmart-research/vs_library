4a98505ce71bb83532caabd0f824c63ba7ea77cf
jtai-dev <jtai.dvlp@gmail.com>
2021-10-03 22:17:38 -0500

    modified requirements and setup

--- setup.py
+++ setup.py
@@ -4,13 +4,27 @@ try:
 except ImportError:
     from distutils.core import setuptools
 
+EGG = '#egg='
+install_requires = []
+
+with open('requirements.txt') as f:
+    for line in f.read().splitlines():
+
+        if line.startswith('git+'):
+            if EGG not in line:
+                raise Exception('egg specification is required.')
+            package_name = line[line.find(EGG) + len(EGG):]
+            dependency_link = line[:line.find(EGG)]
+            install_requires.append(f"{package_name} @ {dependency_link}")
+        else:
+            install_requires.append(line)
 
 config = {
     'description': "Research Tools for Vote Smart",
     'author': "Johanan Tai",
     'author_email': "jtai.dvlp@gmail.com",
     'version': '0.0.2',
-    'install_requires': ['pandas', 'odfpy', 'pg8000','fuzzywuzzy', 'python-Levenshtein', 'tabulate'],
+    'install_requires': install_requires,
     'url': 'https://github.com/votesmart-projects/vs_library',
     'packages': ['vs_library'],
     'name': 'vs_library'
